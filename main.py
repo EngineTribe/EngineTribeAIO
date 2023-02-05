613d46d3ffd69e055b6329fd9edd588598c9f024
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-02-05 08:48:05 +0800

    Update

---
+++ main.py
@@ -0,0 +1,33 @@
+import multiprocessing
+import sys, os
+
+
+def run_api():
+    sys.path.pop(0)
+    sys.path.pop(1)
+    sys.path.insert(0, os.getcwd() + '/enginetribe')
+    print(sys.path)
+    os.environ['CONFIG_PATH'] = os.getcwd() + '/enginetribe/config.yml'
+    import enginetribe
+    enginetribe.run()
+
+
+def run_bot_discord():
+    sys.path.pop(0)
+    sys.path.pop(1)
+    sys.path.insert(0, os.getcwd() + '/enginebot_discord')
+    print(sys.path)
+    os.environ['CONFIG_PATH'] = os.getcwd() + '/enginebot_discord/config.yml'
+    os.environ['LOCALES_PATH'] = os.getcwd() + '/enginebot_discord/locales'
+    import enginebot_discord
+    enginebot_discord.run()
+
+
+if __name__ == '__main__':
+    p_api = multiprocessing.Process(target=run_api)
+    p_api.start()
+    import time
+
+    time.sleep(0.3)
+    p_bot_discord = multiprocessing.Process(target=run_bot_discord)
+    p_bot_discord.start()
